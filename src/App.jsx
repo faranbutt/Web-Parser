8ea9aab2b62c65c6ce5645df29a6a05025d2eb78
faranbutt <faranbutt789@gmail.com>
2023-08-23 22:53:57 +0500

    Pushing web parser frontend

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+   <>
+   <div className="w-screen h-screen bg-yellow-300">
+       Web Parser in React
+       with Vite!
+   </div>
+   </>
+  )
+}
+
+export default App
